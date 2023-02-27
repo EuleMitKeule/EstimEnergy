b598f48ef8ef0cd86b19bfae99609cff0fb1a1f8
Lennard Beers <l.beers@outlook.de>
2023-02-27 15:46:16 +0100

    fix: add long_description

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ setup(
     name='estimenergy_client',
     version='0.1.0',
     description='Client for the Estimenergy API',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
     url='https://github.com/eulemitkeule/estimenergy',
     author='Lennard Beers',
     author_email='l.beers@outlook.de',
