c4e2e6e9d39775e4fea2e6f70217b9d92efdec3f
Lennard Beers <l.beers@outlook.de>
2023-03-02 02:09:26 +0100

    feat: change client endpoints

--- estimenergy_client/client.py
+++ estimenergy_client/client.py
@@ -7,12 +7,22 @@ class EstimEnergyClient:
         self.host = host
         self.port = port
 
-    def get_data(self, name):
-        url = f"http://{self.host}:{self.port}/collector/{name}/data"
+    def get_collector(self, id):
+        url = f"http://{self.host}:{self.port}/collector/{id}"
         response = requests.get(url)
         return response.json()
     
-    async def async_get_data(self, name):
-        url = f"http://{self.host}:{self.port}/collector/{name}/data"
+    async def async_get_collector(self, id):
+        url = f"http://{self.host}:{self.port}/collector/{id}"
+        response = await requests.get(url)
+        return response.json()
+    
+    def get_collectors(self):
+        url = f"http://{self.host}:{self.port}/collector"
+        response = requests.get(url)
+        return response.json()
+    
+    async def async_get_collectors(self):
+        url = f"http://{self.host}:{self.port}/collector"
         response = await requests.get(url)
         return response.json()
