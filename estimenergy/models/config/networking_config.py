3261f7ec85fe2824156291c811f4f42b49e467b3
Lennard Beers <l.beers@outlook.de>
2023-04-28 16:50:58 +0200

    fix: default cors origins

--- estimenergy/models/config/networking_config.py
+++ estimenergy/models/config/networking_config.py
@@ -6,4 +6,7 @@ from estimenergy.const import DEFAULT_HOST, DEFAULT_PORT
 class NetworkingConfig(BaseModel):
     host: str = DEFAULT_HOST
     port: int = DEFAULT_PORT
-    cors_origins: list = []
+    cors_origins: list = [
+        "http://localhost",
+        "http://localhost:12321",
+    ]
